51e2802fb872f5b65ddd1a94593b16c01c0c3c47
Daffa Z <67418594+j3c118124@users.noreply.github.com>
2020-12-02 18:27:29 +0700

    trending data

--- src/App.vue
+++ src/App.vue
@@ -125,9 +125,19 @@ export default {
       ],
       id: "home",
       dropDown: false,
+      trending: [
+        {
+          top: "Trending in TX",
+          title: "Gigi",
+          bottom: "Trending with: Rip Gigi",
+        },
+        { top: "Music", title: "We Won", bottom: "135K Tweets" },
+        { top: "Pop", title: "Blue Ivy", bottom: "40k tweets" },
+        { top: "Trending in US", title: "Denim Day", bottom: "40k tweets" },
+        { top: "Trending", title: "When Beyonce", bottom: "25.4k tweets" },
+      ],
     };
   },
-  components: {},
 };
 </script>
 
